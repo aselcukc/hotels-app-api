423f29615d4c8f0433b71436141a2d6bb012397f
Ali Selcuk Cigir <aselcukc@gmail.com>
2018-05-29 23:52:09 +0300

    scrape obj fix

--- components/Scraper.js
+++ components/Scraper.js
@@ -7,7 +7,7 @@ class Scraper {
     this.count = 1;
   }
 
-  scrapeTripAdvisor(url) {
+  /* scrapeTripAdvisor(url) {
     let arr = [];
 
     return rp({
@@ -49,6 +49,51 @@ class Scraper {
         data[`review_${this.count}`] = obj;
         arr.push(data);
         this.count++;
+      }
+
+      return arr;
+    });
+  } */
+
+  scrapeTripAdvisor(url) {
+    let arr = [];
+
+    return rp({
+      url: url,
+    }).then(body => {
+      const $ = cheerio.load(body);
+      const $wrapper = $('.rev_wrap');
+
+      for (let i = 0; i < 5; i++) {
+        const data = {};
+
+        data.reviewer = $wrapper
+          .find('.info_text')
+          .eq(i)
+          .children()
+          .text();
+        data.reviewDate = $wrapper
+          .find('.ratingDate')
+          .eq(i)
+          .text();
+        data.reviewHeader = $wrapper
+          .find('.noQuotes')
+          .eq(i)
+          .text();
+        data.reviewBody = $wrapper
+          .find('.partial_entry')
+          .eq(i)
+          .text();
+
+        data.reviewLink =
+          'https://www.tripadvisor.com.tr' +
+          $wrapper
+            .find('.quote')
+            .find('a')
+            .attr('href');
+        data.ratingClass = $wrapper.find('.ui_bubble_rating').attr('class');
+
+        arr.push(data);
       }
 
       return arr;
